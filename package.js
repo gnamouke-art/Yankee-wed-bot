2d0ac9e6bd73bc8d85c92fe30eb42de080f3a35f
gnamouke-art <evrardd85@gmail.com>
2026-05-28 21:20:05 +0000

    Initialize package.json for yankee-hells-bot

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "yankee-hells-bot",
+  "version": "3.0.0",
+  "description": "YANKEE HELLS - WhatsApp Bot Multi-User",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@hapi/boom": "^10.0.1",
+    "@whiskeysockets/baileys": "^6.7.5",
+    "axios": "^1.6.2",
+    "chalk": "^4.1.2",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "file-type": "^16.5.4",
+    "fs-extra": "^11.2.0",
+    "libphonenumber-js": "^1.10.51",
+    "node-cache": "^5.1.2",
+    "pino": "^8.17.2",
+    "qrcode": "^1.5.3",
+    "ws": "^8.14.2"
+  }
+}
